c09f5793a8ce883b4db175fda2bf854656372cca
AhmedAGadir <ahmed.gadir@gmail.com>
2022-02-24 13:03:24 +0000

    update

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, RenderResult, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+export const ensureGridApiHasBeenSet = () => {
+  return waitForAsyncCondition(() => {
+      return screen.getByTestId('api').innerHTML !== ''
+  }, 5)
+};
+
+export const waitForAsyncCondition = (condition: () => boolean, maxAttempts: number, attempts=0) => new Promise(function (resolve, reject) {
+  (function waitForCondition() {
+      // we need to wait for the gridReady event before we can start interacting with the grid
+      // in this case we're looking at the api property in our App component, but it could be
+      // anything (ie a boolean flag)
+      if (condition()) {
+          // once our condition has been met we can start the tests
+          return resolve(null);
+      }
+      attempts++;
+
+      if(attempts >= maxAttempts) {
+          reject("Max timeout waiting for condition")
+      }
+
+      // not set - wait a bit longer
+      setTimeout(waitForCondition, 10);
+  })();
 });
+
+describe('test', () => {
+  let {container}: RenderResult = render(<App />);
+
+  beforeAll((done) => {
+    ensureGridApiHasBeenSet().then(() => done(), () => done(new Error("Grid API not set within expected time limits")));
+  })
+
+  // afterEach(() => {
+  //   unmount();
+  // })
+
+  it('all rows selected', () => {
+    let selectAllButton = screen.getByTestId('selectAll');
+    fireEvent.click(selectAllButton);
+    let selectedNodes = container.querySelectorAll('.ag-center-cols-container .ag-row-selected');
+    expect(selectedNodes.length).toBe(3);
+  });
+
+  // it('all rows deselected', () => {
+  //   let deselectAllButton = screen.getByTestId('deselectAll');
+  //   fireEvent.click(deselectAllButton);
+  //   let selectedNodes = container.querySelectorAll('.ag-center-cols-container .ag-row-selected');
+  //   expect(selectedNodes.length).toBe(0);
+  // });
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 import './App.css';
+import { GridApi, GridReadyEvent } from 'ag-grid-community';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [api, setApi] = useState<GridApi>();
+
+   const [rowData] = useState([
+       {make: "Toyota", model: "Celica", price: 35000},
+       {make: "Ford", model: "Mondeo", price: 32000},
+       {make: "Porsche", model: "Boxter", price: 72000}
+   ]);
+   
+   const [columnDefs] = useState([
+       { field: 'make' },
+       { field: 'model' },
+       { field: 'price' }
+   ])
+
+   const onGridReady = (params: GridReadyEvent) => {
+     setApi(params.api);
+   }
+
+   const handleSelectAll = () => {
+    api?.selectAll()
+  };
+
+  const handleDeselectAll = () => {
+    api?.deselectAll()
+  };
+
+   return (
+     <>
+      <p data-testid="api">{api ? 'the grid API has been loaded' : null}</p>
+      <button data-testid="selectAll" onClick={handleSelectAll}>Select All Rows</button>
+      <button data-testid="deselectAll" onClick={handleDeselectAll}>Deselect All Rows</button>
+      <div className="ag-theme-alpine" style={{height: 400, width: 600}}>
+           <AgGridReact
+               rowData={rowData}
+               columnDefs={columnDefs}
+               onGridReady={onGridReady}
+               rowSelection='multiple'
+               >
+           </AgGridReact>
+       </div>
+       </>
+   );
+};
 
 export default App;
